5fb81d0108e13ec6cbf492cc67b6bbd9bcb0bbb4
navadon@gmail.com <navadon@gmail.com>
2018-08-24 09:51:53 +0700

    Add simple btnNumber_Click

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -16,5 +16,15 @@ namespace CPE200Lab1
         {
             InitializeComponent();
         }
+
+        private void btnNumber_Click(object sender, EventArgs e)
+        {
+            string digit = ((Button)sender).Text;
+            if (lblDisplay.Text is "0")
+            {
+                lblDisplay.Text = "";
+            }
+            lblDisplay.Text += digit;
+        }
     }
 }
